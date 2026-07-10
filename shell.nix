d0ae229b95fd87fc0247052f8db247d1612fbad3
simonk <s.kopciewski@dlamnie.net>
2026-07-10 20:47:24 +0200

    Update env

---
+++ shell.nix
@@ -0,0 +1,25 @@
+let
+  pkgs = import <nixpkgs> { };
+
+  projectPackages = with pkgs; [
+    neovim-unwrapped
+    lua
+    stylua
+  ];
+
+  containerScripts = pkgs.project_opencode_helpers_builder {
+    inherit
+      projectPackages
+      pkgs
+      ;
+    projectContainer = "diffconflicts-env";
+    currentDir = toString ./.;
+  };
+in
+pkgs.mkShell {
+  packages =
+    projectPackages
+    ++ (with pkgs; [
+      containerScripts
+    ]);
+}
